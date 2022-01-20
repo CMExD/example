c0167662e87b316a7909705111b22e3249bceb9c
Evo <cmexxd@gmail.com>
2022-01-20 18:46:43 +0300

    script.js mod

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// const obj = {
+//     name: "Jonh",
+//     age: 25,
+//     isMarried: false
+// };
+
+// // console.log(obj.age);
+// console.log(obj.name);
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "да");
+// console.log(typeof(answer));
+
+//const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+//console.log(typeof(answers));
+const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
