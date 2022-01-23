6dec54c56d074bcdb3e42355520048be22c36d4c
Evo <cmexxd@gmail.com>
2022-01-23 19:43:13 +0300

    commit3

--- js/script.js
+++ js/script.js
@@ -1,22 +1,67 @@
 "use strict";
-// a = 15;
-// console.log(a);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres:[],
+    privat: false
+};
+
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
 
-// let number = 5;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+        
+    } else {
+        console.log('error');
+        i--;        
+    }    
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("норм");
+} else if (personalMovieDB.count >= 30) {
+    console.log("киномана");
+} else {
+    console.log("error");
+}
+
+ console.log(personalMovieDB);
 
 
-// const leftBorderWidht = 1;
+// let num = 50;
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
 
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
 
-// number = 10;
-// console.log(number);
-
-// const obj = {
-//     a:50
-// };
-
-// obj.a = 10;
-// console.log(obj);
-
-var name = 'Ivan';
+// for (let i = 1; i < 20; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
+    
