a62420afb4f349a4c07637ffa524764bc05b598b
Evo <cmexxd@gmail.com>
2022-01-26 19:16:07 +0300

    lesson_four

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 "use strict";
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres:[],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres:[],
+//     privat: false
+// };
 
 // const a = prompt('Один из последних просмотренных фильмов?', ''),
 //       b = prompt('На сколько оцените его?', ''),
@@ -18,31 +18,31 @@ const personalMovieDB = {
 // personalMovieDB.movies[a] = b;
 // personalMovieDB.movies[c] = d;
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
         
-    } else {
-        console.log('error');
-        i--;        
-    }    
-}
+//     } else {
+//         console.log('error');
+//         i--;        
+//     }    
+// }
 
-if (personalMovieDB.count < 10) {
-    console.log("мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("норм");
-} else if (personalMovieDB.count >= 30) {
-    console.log("киномана");
-} else {
-    console.log("error");
-}
+// if (personalMovieDB.count < 10) {
+//     console.log("мало фильмов");
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log("норм");
+// } else if (personalMovieDB.count >= 30) {
+//     console.log("киномана");
+// } else {
+//     console.log("error");
+// }
 
- console.log(personalMovieDB);
+//  console.log(personalMovieDB);
 
 
 // let num = 50;
@@ -64,4 +64,29 @@ if (personalMovieDB.count < 10) {
 //     }
 //     console.log(i);
 // }
+
+let num = 20;
     
+function showFirstMessage(text) {
+    console.log(text);
+    num = 10;
+    console.log(num);
+}
+
+showFirstMessage("hello");
+console.log(num);
+
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(3, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
+
+function ret() {
+    let num = 50;
+    return num;
+}
+const anotherNum = ret;
+console.log(anotherNum);
