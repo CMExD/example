cb68865b9b881fe58f1a40925b3b8d6fe1d6c4e9
Evo <cmexxd@gmail.com>
2022-02-06 10:21:34 +0300

    lessonError

--- js/script.js
+++ js/script.js
@@ -1,19 +1,54 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+"use strict";
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres:[],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres:[],
+//     privat: false
+// };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+if (4 == 9) {
+    console.log('Ok!');
+} else {
+    console.log('error');
+}
+
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Too many');
+} else {
+    console.log('ok');
+}
+
+(num === 50) ? console.log('ok') : console.log('error');
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('wrong');
+        break;
+    case 100:
+        console.log('wrong');
+        break;
+    case 50:
+        console.log('yes');
+        break;
+    default:
+        console.log('nothing');
+        break;
+}
